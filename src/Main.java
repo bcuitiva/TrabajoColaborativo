46ea3f72fe02df53da018ce018d4906a2202cd22
Brayan Mendez <brameromrodriguez@gmail.com>
2024-08-26 20:58:10 -0500

    Se agrego la potenciacion

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,15 @@ public class Main {
         //Código de suma y resta
         //Elkin Yamith Almonacid
         //Programa que permite escoger entre multiplicacion y division, pide dos numeros y realiza la operación.
+        //Brayan Yair Mendez Rodriguez
+        //El codigo realiza la potenciación y le pide al usuario tanto la base como el exponente
         //22-08-2024
         Scanner sc = new Scanner (System.in);
         int sum = 0, num = 0, res = 0, op = 0;
         int x, num1, num2, mult=0, div=0;
+        int total=0;
         System.out.println("Que operacion desea hacer: ");
-        System.out.println("1. Suma\n2. Resta\n3.Multiplicacion\n4.Division");
+        System.out.println("1. Suma\n2. Resta\n3.Multiplicacion\n4.Division\n5.Potenciacion");
         op = sc.nextInt();
         switch(op){
             case 1:
@@ -48,6 +51,18 @@ public class Main {
                 num2=sc.nextInt();
                 div=num1/num2;
                 System.out.println("El resultado es: "+div);
+                break;
+            case 5:
+                System.out.println("Ingrese el número base");
+                int base = sc.nextInt();
+                System.out.println("Ingrese el exponente");
+                int exponente = sc.nextInt();
+                total=base;
+                for (int i=2; i<=exponente; i++) {
+                    total= total*base;
+                }
+                System.out.println("Su operación es: " +base+"^"+exponente );
+                System.out.println("RESULTADO: " +total);
                 break;
         }
     }
